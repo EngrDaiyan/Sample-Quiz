838a22cb43b0df29126042c93245303fcc581235
DAIYAN <tttamik.tt@gmail.com>
2023-02-14 00:09:13 +0600

    Night 14

--- app/src/main/java/com/example/samplequiz/Login.java
+++ app/src/main/java/com/example/samplequiz/Login.java
@@ -3,19 +3,12 @@ package com.example.samplequiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-
+        setContentView(R.layout.activity_login);
     }
 }
